bebbb9fb57001d448c7cc2e401d9ce43c0a1cebb
20shaked20 <shakedlevi32@gmail.com>
2024-08-28 17:42:04 +0300

    refresh causes 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Toolbar from './components/Toolbar';
 import HomeScreen from './components/Home/HomeScreen';
